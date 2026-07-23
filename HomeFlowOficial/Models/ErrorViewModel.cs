a1500c1cc11d53683e41dc940a3c089c4f2a991b
jorge <jhernandezcataldo@gmail.com>
2026-07-22 21:20:16 -0400

    Agregar archivos de proyecto.

---
+++ HomeFlowOficial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeFlowOficial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
